00f9e1f1cd520b829186927debe1dff5144a8d22
zhangchi <zhangchi194cm@gmail.com>
2020-05-08 20:16:41 +0800

    add IsSubtree.java; add MaximalSquare.java;

---
+++ IsSubtree.java
@@ -0,0 +1,43 @@
+package practice;
+
+public class IsSubtree {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+
+        return findRoot(s, t);
+    }
+    public boolean findRoot(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        }
+
+        if (s.val == t.val) {
+            if (dfs(s, t)) {
+                return true;
+            }
+        }
+
+        return findRoot(s.left, t) || findRoot(s.right, t);
+    }
+
+    public boolean dfs(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        else if (s == null || t == null) {
+            return false;
+        }
+
+        if (s.val != t.val) {
+            return false;
+        }
+
+        return dfs(s.left, t.left) && dfs(s.right, t.right);
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x;}
+    }
+}
---
+++ MaximalSquare.java
@@ -0,0 +1,26 @@
+package practice;
+
+public class MaximalSquare {
+    public int maximalSquare(char[][] matrix) {
+        int square = 0;
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i == 0 || j == 0) && matrix[i][j] == '1') {
+                    dp[i][j] = 1;
+                } else if (matrix[i][j] == '1') {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
+                square = Math.max(square, dp[i][j] * dp[i][j]);
+            }
+        }
+        return square;
+    }
+}
