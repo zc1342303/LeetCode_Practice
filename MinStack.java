74c71936c4353723480cfa55806a9f436fb1a308
zhangchi <zhangchi194cm@gmail.com>
2020-05-12 01:09:00 +0800

    add MinStack.java;

---
+++ MinStack.java
@@ -0,0 +1,30 @@
+package practice;
+
+import java.util.*;
+
+public class MinStack {
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
+    public MinStack() {
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.empty() || x <= minStack.peek())
+            minStack.push(x);
+    }
+
+    public void pop() {
+        int del = stack.pop();
+        if (del == minStack.peek())
+            minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
