513f744ad6a6dd18814d9390fcfd9ff677d2d6ad
zhangchi <zhangchi194cm@gmail.com>
2020-05-14 19:28:57 +0800

    add SingleNumber.java;

---
+++ SingleNumber.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num: nums) {
+            res ^= num;
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        SingleNumber solution = new SingleNumber();
+        int[] nums = {2, 2, 1};
+        int ans = solution.singleNumber(nums);
+        System.out.println(ans);
+    }
+}
